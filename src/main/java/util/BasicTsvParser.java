a9a89a96104c4ebcf75ac27226f45900e0dde2f7
maigumigu <starandpoem143@gmail.com>
2019-10-27 16:40:35 +0900

    implemented wordTsvParser

--- src/main/java/util/BasicTsvParser.java
+++ src/main/java/util/BasicTsvParser.java
@@ -1,12 +1,5 @@
 package main.java.util;
 
-import com.univocity.parsers.tsv.TsvParser;
-import com.univocity.parsers.tsv.TsvParserSettings;
-
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.List;
 
