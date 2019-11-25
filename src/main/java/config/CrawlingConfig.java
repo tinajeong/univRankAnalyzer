c48ec76269d03691d87b46e4e8ae12389b1310f2
migujeong@gmail.com <migujeong@gmail.com>
2019-11-25 17:43:40 +0900

    modified CrawlingConfig

--- src/main/java/config/CrawlingConfig.java
+++ src/main/java/config/CrawlingConfig.java
@@ -6,6 +6,7 @@ public class CrawlingConfig {
     public final static String userAgentDefault = "Mozilla/5.0";
     public final static String userAgentLinux = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) snap Chromium/78.0.3904.97 Chrome/78.0.3904.97 Safari/537.36";
     public final static String userAgentMac = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.75 Safari/537.36";
+    public final static String userAgentWindow = "";
     public final static String crawler4jUserAgent = "crawler4j (http://code.google.com/p/crawler4j/)";
     public final static int pageNum = 10;
 
