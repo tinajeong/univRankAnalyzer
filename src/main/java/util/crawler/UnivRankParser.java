d49170aae7dd350422a50bdc008687e54ff7b566
migujeong@gmail.com <migujeong@gmail.com>
2019-12-12 12:56:56 +0900

    Make skeleton for crawler

--- src/main/java/util/crawler/UnivRankParser.java
+++ src/main/java/util/crawler/UnivRankParser.java
@@ -40,25 +40,14 @@ public class UnivRankParser implements Crawler {
     }
 
     public void crawlingUnivRankPage(String url) throws IOException {
-        Document doc = Jsoup.connect(url)
-                .header("User-Agent", CrawlingConfig.userAgentDefault)
-                .timeout(3000)
-                .maxBodySize(Integer.MAX_VALUE)
-                .get();
+        //TODO
+        Document doc = null;
 
         Elements elements = doc.select("#resultsMain .sep");
 
         for (Element element : elements) {
             UnivRankDTO univRankDTO = new UnivRankDTO();
-            Long rank = Long.parseLong(element.select(".rankscore-bronze").text().replaceAll("[^0-9]", ""));
-            univRankDTO.setRank(rank);
-
-            String univName = element.child(2).select(".h-taut a").text().replaceAll("[^A-Za-z ]"," ").trim();
-
-            univRankDTO.setUnivName(univName);
-            univRankDTO.setCountry(element.child(2).select(".t-taut span").first().text());
-            univRankDTO.setUnivInfoHref(element.child(2).select(".h-taut a").attr("href"));
-
+            //TODO
             univRankDTOS.add(univRankDTO);
         }
     }
